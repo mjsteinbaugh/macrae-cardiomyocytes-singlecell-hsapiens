fb18247c398748ff8070a554e26ddc35726ab926
Michael Steinbaugh <mike@steinbaugh.com>
2018-07-30 12:16:02 -0400

    Reorganize bcbioRNASeq script order

---
+++ 01_bcbioSingleCell.R
@@ -0,0 +1,13 @@
+# v0.1.18
+library(bcbioSingleCell)
+bcb <- bcbioSingleCell(
+    uploadDir = "bcbio_harvard_indrop_v3/final",
+    organism = "Homo sapiens",
+    interestingGroups = "genotype",
+    sampleMetadataFile = "meta/multiplexed_sample_metadata.xlsx",
+    genomeBuild = "GRCh38",
+    ensemblRelease = 90
+)
+# Back up all data inside bcbio object
+flatFiles <- flatFiles(bcb)
+saveData(bcb, flatFiles, dir = file.path("data", Sys.Date()))
