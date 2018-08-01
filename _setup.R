34122ef17f7ed79a377839dc2f80a638787fde6d
Michael Steinbaugh <mike@steinbaugh.com>
2018-08-01 15:40:42 -0400

    Update scripts

---
+++ _setup.R
@@ -0,0 +1,26 @@
+library(bcbioSingleCell)
+library(Seurat)
+library(knitr)
+library(rmarkdown)
+library(tidyverse)
+
+# Set seed for reproducibility
+set.seed(1454944673L)
+
+opts_chunk[["set"]](
+    audodep = TRUE,
+    cache = TRUE,
+    cache.lazy = FALSE,
+    dev = c("png", "pdf"),
+    fig.height = 10L,
+    fig.retina = 2L,
+    fig.width = 10L,
+    tidy = FALSE
+)
+
+theme_set(
+    theme_paperwhite(
+        base_size = 14L,
+        legend_position = "right"
+    )
+)
